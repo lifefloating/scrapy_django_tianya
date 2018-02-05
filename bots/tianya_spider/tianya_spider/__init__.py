d1ef946638e62826f49313cbf34a90d90aaf941d
yqiqi <imshuazi@126.com>
2018-02-05 13:35:33 +0800

    Delete __init__.py

--- bots/tianya_spider/tianya_spider/__init__.py
+++
@@ -1,6 +0,0 @@
-import os, sys, django
-
-sys.path.append(r'E:\PythonProject\scrapy_django_tianya2\tianya')  # 添加django项目路径到pythonpath
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tianya.settings")  # django项目.settings
-
-django.setup()
